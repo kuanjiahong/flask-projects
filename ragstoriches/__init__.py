2768b315448ab8b7457cc81b14f64dadc91f7fa1
jiahong <kuanjiahong@gmail.com>
2022-01-26 12:32:51 +0800

    Changed env settings

--- ragstoriches/__init__.py
+++ ragstoriches/__init__.py
@@ -5,16 +5,13 @@ from flask import Flask, render_template, url_for
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    
+
     if app.config["ENV"] == 'production':
         app.config.from_pyfile('prod_settings.cfg', silent=True)
     elif app.config["ENV"] == 'development':
         app.config.from_pyfile('dev_settings.cfg', silent=True)
     
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
+    if test_config is not None:
         # load the test config if passed in
         app.config.from_mapping(test_config)
     
