9852906e689fc50eea56bc65570bd64c749523f0
jiahong <kuanjiahong@gmail.com>
2022-01-28 18:14:10 +0800

    Updated fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,10 +6,15 @@ from werkzeug.security import generate_password_hash
 
 from datetime import datetime
 
-random_users = [
-    {"name": "test1", "email": "test1@example.com", "password": generate_password_hash("123456"), "created_at": datetime.now() },
-    {"name": "test2", "email": "test2@example.com", "password": generate_password_hash("helloworld"), "created_at": datetime.now()},
-]
+random_user =  {
+    "name": "test1", 
+    "email": "test1@example.com", 
+    "password": generate_password_hash("123456"), 
+    "progress": 0,
+    "created_at": datetime.now(),
+    "last_modified": datetime.now()
+    }
+
 
 
 @pytest.fixture
@@ -19,6 +24,6 @@ def client():
     with app.test_client() as c:
         with app.app_context():
             db = ragstoriches.database.get_db()
-            db.user.insert_many(random_users)
+            g.oid = str(db.user.insert_one(random_user).inserted_id)
             yield c
             db.user.delete_many({})
