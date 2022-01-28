b6914b9fef7b24d21c0b7dd13f57b52dd3e6abf1
jiahong <kuanjiahong@gmail.com>
2022-01-28 18:14:18 +0800

    Added more test cases

--- tests/test_database.py
+++ tests/test_database.py
@@ -22,3 +22,35 @@ def test_check_user_with_correct_password_but_wrong_details(client):
 
 def test_add_user(client):
     assert ragstoriches.database.add_user("newtester", "newtester@example.com", "newtester666")
+
+def test_get_user_by_oid(client):
+    assert ragstoriches.database.get_user_by_oid(g.oid) is not None
+
+def test_add_character(client):
+    assert ragstoriches.database.add_character(g.oid, "John Cena", 1000) is True
+
+def test_update_progress(client):
+    assert ragstoriches.database.update_progress(g.oid) is True
+
+    result = ragstoriches.database.get_user_by_oid(g.oid)
+    assert result['progress'] > 0
+
+def test_update_character_fund_and_progress(client):
+    ragstoriches.database.add_character(g.oid, "John Cena", 1000)
+    assert ragstoriches.database.update_character_fund_and_progress(g.oid, 2000) is True   
+    result = ragstoriches.database.get_user_by_oid(g.oid)
+    assert result['character']['name'] == "John Cena"
+    assert result['character']['funds'] == 2000
+    assert result['progress'] == 1
+
+def test_update_end(client):
+    ragstoriches.database.add_character(g.oid, "John Cena", 1000)
+    assert ragstoriches.database.update_end(g.oid) is True
+    result = ragstoriches.database.get_user_by_oid(g.oid)
+    assert result['end'] == True
+
+def test_delete_character(client):
+    ragstoriches.database.add_character(g.oid, "John Cena", 1000)
+    assert ragstoriches.database.delete_character(g.oid) is True
+    result = ragstoriches.database.get_user_by_oid(g.oid)
+    assert result.get('character') is None
